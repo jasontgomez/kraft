5184dfb9d70dd3027fab47b0c86756815b049a26
jasontgomez <jason.gomez@mygeorgian.ca>
2023-12-04 22:59:44 -0500

    finished products page and updated header

--- scripts.js
+++ scripts.js
@@ -4,17 +4,20 @@ carousel();
 function carousel() {
     let i;
     const slides = document.getElementsByClassName("mySlides");
+    // loop through, turn off all images
     for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+        slides[i].style.display = "none";
     }
     slideIndex++;
+    // show current image
     if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
+        slides[slideIndex - 1].style.display = "block";
+     // transition function   
     setTimeout(() => {
-    for (i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("fade");
-    }
-    slides[slideIndex - 1].classList.add("fade");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("fade");
+        }
+        slides[slideIndex - 1].classList.add("fade");
     }, 100);
     setTimeout(carousel, 5000); // Change image every 5 seconds
 }
